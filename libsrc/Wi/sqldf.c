danielhmills/virtuoso-opensource

commit fb0cf1cdd1ec20e226d8f0eb41710eaf8093437b

Fixed issue with bad index op ref in table dft (fixes #1190, #1191)

libsrc/Wi/sqldf.c
@@ -1443,6 +1443,8 @@ dfe_inx_op_col_def_table (df_inx_op_t * dio, df_elt_t * col_dfe, df_elt_t * exce
   if (dio->dio_table
       && dfe_defines (dio->dio_table, col_dfe))
     return dio->dio_table;
+  if (dio->dio_terms && !IS_BOX_POINTER(dio->dio_terms))
+    return NULL;
   DO_SET  (df_inx_op_t *, term, &dio->dio_terms)
     {
       df_elt_t *def_dfe = dfe_inx_op_col_def_table (term, col_dfe, except_tb);
@@ -7509,6 +7511,9 @@ dfe_body_copy (sqlo_t * so, df_elt_t * super, df_elt_t * parent)
     }
   else
     {
+      if (!super->_.sub.first)
+        sqlc_new_error (so->so_sc->sc_cc, "42000", "SQI03", "Internal error in SQL compiler: sub.first = 0");
+
       for (elt = super->_.sub.first->dfe_next; elt; elt = elt->dfe_next)
 	{
 	  df_elt_t * copy_elt = sqlo_layout_copy_1 (copy_super->dfe_sqlo, elt, copy_super);
@@ -7551,6 +7556,8 @@ inx_op_copy (sqlo_t * so, df_inx_op_t * dio,
   memcpy (copy, dio, sizeof (df_inx_op_t));
   if (dio->dio_table == org_tb_dfe)
     copy->dio_table = tb_dfe;
+  if (dio->dio_terms && !IS_BOX_POINTER(dio->dio_terms))
+    sqlc_new_error (so->so_sc->sc_cc, "42000", "SQI05", "Internal error in SQL compiler: non inx op in inx copy");
   else if (dio->dio_table)
     {
       copy->dio_table = sqlo_layout_copy_1 (so, dio->dio_table, NULL);

commit 43db0a8d6deedbcda04a7a532604da1e6d3a6fd4

Fixed issue with bad index op ref in table dft (fixes #1245)
Moved check for dio_terms into the correct block.

This fixes an issue introduced by commit
fb0cf1cdd1ec20e226d8f0eb41710eaf8093437b

libsrc/Wi/sqldf.c
@@ -7557,8 +7557,6 @@ inx_op_copy (sqlo_t * so, df_inx_op_t * dio,
   memcpy (copy, dio, sizeof (df_inx_op_t));
   if (dio->dio_table == org_tb_dfe)
     copy->dio_table = tb_dfe;
-  if (dio->dio_terms && !IS_BOX_POINTER(dio->dio_terms))
-    sqlc_new_error (so->so_sc->sc_cc, "42000", "SQI05", "Internal error in SQL compiler: non inx op in inx copy");
   else if (dio->dio_table)
     {
       copy->dio_table = sqlo_layout_copy_1 (so, dio->dio_table, NULL);
@@ -7566,6 +7564,8 @@ inx_op_copy (sqlo_t * so, df_inx_op_t * dio,
   else if (dio->dio_terms)
     {
       s_node_t *iter;
+      if (!IS_BOX_POINTER(dio->dio_terms))
+        sqlc_new_error (so->so_sc->sc_cc, "42000", "SQI05", "Internal error in SQL compiler: non inx op in inx copy");
       copy->dio_terms = t_set_copy (dio->dio_terms);
       DO_SET_WRITABLE (df_inx_op_t *, term, iter, &copy->dio_terms)
 	{